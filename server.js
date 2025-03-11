49584cdb6451f45989532c63b1d7145672e6fb74
Chayan Neogi <136154400+Chayanethic@users.noreply.github.com>
2025-03-11 13:36:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS middleware
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://quiz-frontend-final.vercel.app/');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
